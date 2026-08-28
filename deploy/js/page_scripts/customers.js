47a648a515d9629b33920243bfe942defd70de60
jsmalc-algebra <jsmalc2@algebra.hr>
2026-08-28 16:49:53 +0200

    Fixed an overlooked bug and cloned the fix to deployment

--- deploy/js/page_scripts/customers.js
+++ deploy/js/page_scripts/customers.js
@@ -243,7 +243,7 @@ function addViewDetailsColumn() {
 function addNewCustomerButton() {
     const topRow = document.getElementById('top-row');
 
-    topRow.innerHTML +=
+    topRow.insertAdjacentHTML('beforeend',
         `<a href="add-customer.html" class="btn btn-primary">
             NEW CUSTOMER
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle"
@@ -252,7 +252,7 @@ function addNewCustomerButton() {
                 <path
                     d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
             </svg>
-        </a>`
+        </a>`)
 }
 
 function authenticatedUserRender(){
